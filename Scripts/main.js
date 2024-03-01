d8b25e44abb7b3d4fcea7fe141535e4b9e551d2e
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-01 08:45:30 +0200

    add tasks

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,8 +5,14 @@ let input =document.getElementById("input")
 
 addButton.addEventListener("click",function(){
     const task = document.createElement("p")
+    task.classList.add("task-styling")
     task.innerText=input.value
-    toDoContainer.appendChild(task)
     
+    toDoContainer.appendChild(task)
+    input.value=""
+    task.addEventListener("dblclick",function(){
+        toDoContainer.removeChild(task)
+    })
+
 
 })
